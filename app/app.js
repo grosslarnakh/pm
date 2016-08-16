192d3923f3f742fbc6820a6b89a00e8fc1a26256
Alex <a.aleksu@gmail.com>
2016-08-16 21:39:58 +0300

    set up and fix scroll directive

--- app/app.js
+++ app/app.js
@@ -9,37 +9,53 @@ requirejs([prefpath + 'app/vendor.min.js'], function() {
 });
 
 function loadDependencies() {
-requirejs([
-    //'app/vendor.min',
-    'app/controllers/root_ctrl',
-    'app/controllers/notice_ctrl',
-    'app/controllers/home_ctrl',
-    'app/controllers/modals/add_ctrl',
-    'app/controllers/modals/startDate.client.controller',
-    'app/controllers/modals/startValue.client.controller',
-    'app/models/weeks_provider',
-    'app/models/week',
-    'app/models/day',
-    'app/models/payment',
-    'app/ui-bootstrap-tpls-0.11.2.min'
-], function() {
-    //require(['app/ui-bootstrap-tpls-0.11.2.min']);
-    var pm = angular.module('pm', ['ngResource', 'ngAnimate', 'ui.router', 'ui.utils', 'ui.bootstrap', 'ui.bootstrap.modal', 'ui.bootstrap.tpls']);
-    
-    pm.factory('Day', day);
-    pm.factory('Week', week);
-    pm.factory('WeeksProvider', weeksProvider);
-    pm.factory('Payment', payment);
+    requirejs([
+        //'app/vendor.min',
+        'app/controllers/root_ctrl',
+        'app/controllers/notice_ctrl',
+        'app/controllers/home_ctrl',
+        'app/controllers/modals/add_ctrl',
+        'app/controllers/modals/startDate.client.controller',
+        'app/controllers/modals/startValue.client.controller',
+        'app/models/weeks_provider',
+        'app/models/week',
+        'app/models/day',
+        'app/models/payment',
+        'app/ui-bootstrap-tpls-0.11.2.min'
+    ], function() {
+        //require(['app/ui-bootstrap-tpls-0.11.2.min']);
+        var pm = angular.module('pm', ['ngResource', 'ngAnimate', 'ui.router', 'ui.utils', 'ui.bootstrap', 'ui.bootstrap.modal', 'ui.bootstrap.tpls']);
+        
+        pm.factory('Day', day);
+        pm.factory('Week', week);
+        pm.factory('WeeksProvider', weeksProvider);
+        pm.factory('Payment', payment);
 
-    pm.controller('rootCtrl', rootCtrl);
-    pm.controller('noticeCtrl', noticeCtrl);
-    pm.controller('homeCtrl', homeCtrl);
-    pm.controller('addCtrl', addCtrl);
-    pm.controller('StartDateController', startDateCtrl);
-    pm.controller('StartValueController', startValueCtrl);
-    
-    angular.element(document).ready(function() {
-        angular.bootstrap(document, ['pm']);
+        pm.controller('rootCtrl', rootCtrl);
+        pm.controller('noticeCtrl', noticeCtrl);
+        pm.controller('homeCtrl', homeCtrl);
+        pm.controller('addCtrl', addCtrl);
+        pm.controller('StartDateController', startDateCtrl);
+        pm.controller('StartValueController', startValueCtrl);
+
+        console.log('!!');
+
+        pm.directive('onScroll', function($window){
+            return function(scope, element, attrs){
+                angular.element($window).bind('scroll', function(){
+                    if(this.pageYOffset > 200) {
+                        scope.fixed_toolbar = true;
+                    }
+                    else {
+                        scope.fixed_toolbar = false;
+                    }
+                });
+            }
+        });
+        
+        angular.element(document).ready(function() {
+            angular.bootstrap(document, ['pm']);
+        });
     });
-});
 }
+
